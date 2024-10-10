bae56040bbe4400e87ed771421706c49ef0af3c2
Gabriel Moura <gabrielmsg@insiberia.net>
2024-10-10 01:30:17 -0300

    Atualização do compile.sh e (finalmente) arquivo binário independente para linux

--- compile.sh
+++ compile.sh
@@ -3,3 +3,7 @@
 javac -d out/ src/br/edu/ufpel/cache_simulator/*/*.java src/br/edu/ufpel/cache_simulator/*.java
 cd out
 jar cvfm ../Simulator.jar Simulator.mf br/edu/ufpel/cache_simulator/*/*.class br/edu/ufpel/cache_simulator/*.class
+cd ..
+echo '#!/usr/bin/java -jar' > linux/cache_simulator
+cat Simulator.jar >> linux/cache_simulator
+chmod +x linux/cache_simulator
